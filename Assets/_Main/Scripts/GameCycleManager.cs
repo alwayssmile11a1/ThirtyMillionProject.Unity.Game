d4437f1b40be342b4f7bce54d6724cb832f1129b
2SMILE2 <alwayssmile11a1@gmail.com>
2018-05-08 23:12:26 +0700

    core game

---
+++ Assets/_Main/Scripts/GameCycleManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class GameCycleManager : MonoBehaviour {
+
+
+    public static GameCycleManager Instance
+    {
+        get
+        {
+            if (m_Instance != null) return m_Instance;
+
+            m_Instance = FindObjectOfType<GameCycleManager>();
+
+            if (m_Instance != null) return m_Instance;
+
+            GameObject gameObject = new GameObject("GameManager");
+            gameObject = Instantiate(gameObject);
+            m_Instance = gameObject.AddComponent<GameCycleManager>();
+           
+
+            return m_Instance;
+        }
+            
+    }
+
+    private static GameCycleManager m_Instance;
+
+    public CinemachineVirtualCamera virtualMainCamera;
+    public CinemachineVirtualCamera wholeSceneVirtualCamera;
+
+    [System.Serializable]
+    public class Path
+    {
+        public GameObject spawnObject;
+        public Transform spawnTransform;
+        public Collider destination;
+    }
+
+    public Path[] paths;
+
+    public List<VehicleBehaviour> vehicles = new List<VehicleBehaviour>();
+
+
+    private int m_CurrentIndex;
+
+    // Use this for initialization
+    void Awake () {
+
+        if(Instance!=this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            wholeSceneVirtualCamera.enabled = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            wholeSceneVirtualCamera.enabled = false;
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            SpawnNewPath();
+
+            SwitchBackToOriginalCamera();
+
+        }
+
+    }
+
+    public void SpawnNewPath()
+    {
+        if (m_CurrentIndex >= paths.Length) return;
+
+
+        VehicleBehaviour vehicle = Instantiate(paths[m_CurrentIndex].spawnObject, paths[m_CurrentIndex].spawnTransform.position, paths[m_CurrentIndex].spawnTransform.rotation).GetComponent<VehicleBehaviour>();
+
+
+        vehicle.destinationCollider = paths[m_CurrentIndex].destination;
+
+
+        vehicles.Add(vehicle);
+
+        virtualMainCamera.Follow = vehicle.transform;
+
+
+        PlayBackOtherVehicles();
+
+    }
+
+
+
+    private void SwitchToWholeSceneCamera()
+    {
+        wholeSceneVirtualCamera.enabled = true;
+    }
+
+    private void SwitchBackToOriginalCamera()
+    {
+        wholeSceneVirtualCamera.enabled = false;
+    }
+    
+    private void PlayBackOtherVehicles()
+    {
+        for (int i = 0; i < vehicles.Count - 1; i++)
+        {
+            vehicles[i].gameObject.SetActive(true);
+            vehicles[i].PlayBack();
+        }
+    }
+
+    private void EndPlayBack()
+    {
+        for (int i = 0; i < vehicles.Count - 1; i++)
+        {
+            vehicles[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void StartPath()
+    {
+        SwitchBackToOriginalCamera();
+    }
+
+    public void EndPath()
+    {
+        SwitchToWholeSceneCamera();
+        EndPlayBack();
+        m_CurrentIndex++;
+    }
+
+
+
+}
--- Assets/_Main/Scripts/VehicleBehaviour.cs
+++ Assets/_Main/Scripts/VehicleBehaviour.cs
@@ -8,56 +8,55 @@ public class VehicleBehaviour : MonoBehaviour {
     public LayerMask bounceLayerMask;
     public LayerMask deadLayerMask;
 
-
+    
     public float speed = 10f;
 
     public float raycastDistance = 0.2f;
 
     public Transform[] rayOrigins;
 
-    //private float m_Horizontal;
-    //private float m_Vertical;
-    //private Vector3 m_ForceVector;
-    //public float maxSpeed = 10f;
-    //public float force = 5f;
+    [HideInInspector]
+    public Collider destinationCollider;
 
-    class RecordInfo
-    {
-        public float timer;
-        public Vector3 velocity;
-        public Quaternion rotation;
-    }
-
+    //class RecordInfo
+    //{
+    //    public float timer;
+    //    public Vector3 velocity;
+    //    public Quaternion rotation;
+    //}
+    //private List<RecordInfo> m_Record = new List<RecordInfo>();
 
     private Rigidbody m_RigidBody;
 
     private bool m_IsPlayingBack = false;
 
-    private List<RecordInfo> m_Record = new List<RecordInfo>();
+
+    private List<Vector3> m_RecordPositions = new List<Vector3>();
 
     private float m_Timer;
 
     private Vector3 m_OriginalPosition;
     private Quaternion m_OriginalRotation;
 
+    private bool m_IsEnded;
+
 	void Awake () {
         m_RigidBody = GetComponent<Rigidbody>();
         m_OriginalPosition = transform.position;
         m_OriginalRotation = transform.rotation;
+      
     }
 
 
     private void Update()
     {
-        //m_Horizontal = Input.GetAxis("Horizontal");
-        //m_Vertical = Input.GetAxis("Vertical");
 
-        //m_ForceVector = new Vector3(0, 0, m_Vertical);
+        if (m_IsEnded) return;
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SaveRecord();
-            StartCoroutine(PlayBack());
+            StartCoroutine(InternalPlayBack());
         }
 
 
@@ -71,8 +70,6 @@ public class VehicleBehaviour : MonoBehaviour {
 
     void FixedUpdate()
     {
-        //if (m_IsPlayingBack) return;
-
         Move();
     }
 
@@ -106,10 +103,34 @@ public class VehicleBehaviour : MonoBehaviour {
     {
         if (deadLayerMask.Contains(collision.gameObject))
         {
-            transform.position = new Vector3(5.35f, 1.2f, -22.38f);
+            transform.position = m_OriginalPosition;
+            transform.rotation = m_OriginalRotation;
+            m_RigidBody.velocity = Vector3.zero;
         }
 
         //ChangeDirection(collision.contacts[0].normal);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == destinationCollider)
+        {
+            End();
+        }
+    }
+
+    private void End()
+    {
+        if (!m_IsEnded)
+        {
+            SaveRecord();
+            GameCycleManager.Instance.EndPath();
+            gameObject.layer = LayerMask.NameToLayer("NoCollider");
+            m_IsEnded = true;
+        }
+
+        gameObject.SetActive(false);
     }
 
 
@@ -128,15 +149,18 @@ public class VehicleBehaviour : MonoBehaviour {
     private void SaveRecord()
     {
 
-        RecordInfo info = new RecordInfo();
-        info.timer = m_Timer;
-        info.rotation = transform.rotation;
-        m_Record.Add(info);
+        //RecordInfo info = new RecordInfo();
+        //info.timer = m_Timer;
+        //info.rotation = transform.rotation;
+        //m_Record.Add(info);
 
-        m_Timer = 0;
+        //m_Timer = 0;
+
+        m_RecordPositions.Add(transform.position);
+
     }
 
-    private IEnumerator PlayBack()
+    private IEnumerator InternalPlayBack()
     {
         m_IsPlayingBack = true;
 
@@ -146,30 +170,55 @@ public class VehicleBehaviour : MonoBehaviour {
 
         int i = 0;
 
-        while (i < m_Record.Count)
+        //while (i < m_Record.Count)
+        //{
+
+        //    if (m_Record[i].timer > 0)
+        //    {
+        //        m_Record[i].timer -= Time.deltaTime;
+
+        //        transform.rotation = m_Record[i].rotation;
+
+        //        if (m_Record[i].timer <= 0)
+        //        {
+        //            i++;
+        //        }
+
+        //    }
+
+        //    yield return null;
+        //}
+
+        while(i<m_RecordPositions.Count)
         {
 
-            if (m_Record[i].timer > 0)
+            if((m_RecordPositions[i]-transform.position).sqrMagnitude <=0.1f)
             {
-                m_Record[i].timer -= Time.deltaTime;
+                i++;
 
-                transform.rotation = m_Record[i].rotation;
+                if (i == m_RecordPositions.Count) break;
 
-                if (m_Record[i].timer <= 0)
-                {
-                    i++;
-                }
+                Vector3 direction = (m_RecordPositions[i] - transform.position).normalized;
+
+                transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
 
             }
+            else
+            {
+                Vector3 direction = (m_RecordPositions[i] - transform.position).normalized;
+                transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+            }
+
 
             yield return null;
         }
 
-
-
         m_IsPlayingBack = false;
     }
 
-
+    public void PlayBack()
+    {
+        StartCoroutine(InternalPlayBack());
+    }
 
 }
